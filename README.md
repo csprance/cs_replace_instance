b4d222f35512f13aeada61b48b2fe50d708b7362
Chris Sprance <chrissprance@gmail.com>
2018-10-15 02:53:25 -0400

    Initial Commit :star:

---
+++ README.md
@@ -0,0 +1,17 @@
+# Replace Instance
+> A modo kit to replace an instance source with a different mesh
+
+## Usage
+* Select Instance(s)
+* Select source mesh to replace instances with
+* Press replace instance button or run `cs_replace_instance.run` 
+
+## Install
+* Drag and drop cs_replace_instance folder into Modo kits folder
+    * Open Modo
+    * System -> Open Kits Folder
+    * Drag and Drop
+    * Restart Modo
+    
+## Other
+Based on the kit by Muha on the foundry forums.
---
+++ lxserv/commander_breakfast.py
@@ -0,0 +1,33 @@
+import lx, modo, cs_replace_instance
+
+"""A simple example of a blessed MODO command using the commander module.
+https://github.com/adamohern/commander for details"""
+
+
+class CommandClass(cs_replace_instance.CommanderClass):
+
+    def commander_arguments(self):
+        return [
+            {
+                'name': 'dish1',
+                'datatype': 'string',
+                'default': 'bacon',
+                'values_list_type': 'popup',
+                'values_list': ['bacon', 'quinoa']
+            }, {
+                'name': 'dish2',
+                'datatype': 'string',
+                'default': 'eggs',
+                'values_list_type': 'sPresetText',
+                'values_list': ['eggs', 'kale']
+            }
+        ]
+
+    def commander_execute(self, msg, flags):
+        dish1 = self.commander_arg_value(0)
+        dish2 = self.commander_arg_value(1)
+
+        modo.dialogs.alert("breakfast", ' and '.join([dish1, dish2]))
+
+
+lx.bless(CommandClass, 'cs_replace_instance.breakfast')
